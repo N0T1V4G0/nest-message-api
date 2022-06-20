378740d6afb74d4b98c6594726dba3821a896b8c
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-06-20 00:17:10 -0300

    feat: create global pipe & message dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,20 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDTO } from './dtos/CreateMessageDTO';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   async listMessages() {
-    return 'listMessages';
+    return { message: 'not implemented' };
   }
 
   @Post()
-  async createMessage(@Body() body) {
-    console.log(body);
-    return 'createMessage';
+  async createMessage(@Body() body: CreateMessageDTO) {
+    return { message: 'not implemented', body };
   }
 
   @Get('/:id')
   async getMessage(@Param('id') id) {
-    console.log(id);
-    return 'getMessage';
+    return { message: 'not implemented', id };
   }
 }
